cd3e5b9030b96d2e7468f000abd6aeec05430b66
Styopa Perov <108626279+loonategjr@users.noreply.github.com>
2023-04-18 17:04:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
 import {Route, Routes} from 'react-router-dom'
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
+import ProductItem from "./components/ProductItem/ProductItem";
 
 function App() {
     const {onToggleButton, tg} = useTelegram();
@@ -19,8 +20,13 @@ function App() {
             <Routes>
                 <Route index element={<ProductList />}/>
                 <Route path={'form'} element={<Form />}/>
+                
                 <Route path={'1'} element={'Страница первого товара'}/>
-                <Route path={'2'} element={'Страница второго товара'}/>
+                <Route path={'2'} element={<ProductItem
+                    product={item}
+                    onAdd={redirect}
+                    className={'item'}
+                />}/>
             </Routes>
         </div>
     );
