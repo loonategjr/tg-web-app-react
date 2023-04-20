3bfdd8d041ddf56336cca729020a45cc46a2dbca
Styopa Perov <108626279+loonategjr@users.noreply.github.com>
2023-04-20 16:49:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import {Route, Routes} from 'react-router-dom'
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 import Item from "./components/Item/Item";
-import ProductItem from "./components/ProductItem/ProductItem";
-
+import BrandList from "./components/BrandList/BrandList";
 function App() {
     const {tg} = useTelegram();
 
@@ -19,16 +18,12 @@ function App() {
         <div className="App">
             <Header />
             <Routes>
-                <Route index element={<ProductList />}/>
+                <Route index element={<BrandList />}/>
                 <Route path={'form'} element={<Form />}/>
-                <Route path={'0'} element={<Item />}/>
-                <Route path={'1'} element={<Item />}/>
-                <Route path={'2'} element={<Item />}/>
-                <Route path={'3'} element={<Item />}/>
-                <Route path={'4'} element={<Item />}/>
-                <Route path={'5'} element={<Item />}/>
-                <Route path={'6'} element={<Item />}/>
-                <Route path={'7'} element={<Item />}/>
+                <Route path={'0'} element={<ProductList />}/>
+                <Route path={'0/0'} element={<Item />}/>
+                <Route path={'0/1'} element={<Item />}/>
+
 
             </Routes>
         </div>
