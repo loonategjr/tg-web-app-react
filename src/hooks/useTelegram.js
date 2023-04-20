0b9151118bfb8602c87ec30f426ae088ba387a8d
Styopa Perov <108626279+loonategjr@users.noreply.github.com>
2023-04-20 20:13:10 +0300

    Update useTelegram.js

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,25 +1,19 @@
 const tg = window.Telegram.WebApp;
 
 const products = [
-    {id: '0', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '1', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '2', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '3', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '4', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '5', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '6', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '7', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    [
+        {id: '0', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+        {id: '1', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
+    ],
+    [
+        {id: '0', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
+        {id: '1', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+    ]
 ]
 
 const brands = [
     {id: '0', title: 'бренд1', price: 5000, description: 'Синего цвета, прямые'},
     {id: '1', title: 'бренд2', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '2', title: 'бренд3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '3', title: 'бренд4', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '4', title: 'бренд5', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '5', title: 'бренд6', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '6', title: 'бренд7', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '7', title: 'бренд8', price: 12000, description: 'Зеленого цвета, теплая'},
 ]
 
 
@@ -37,6 +31,8 @@ export function useTelegram() {
     }
 
     const onClose = () => {
+        alert(cururls);
+        alert()
         window.location.replace(newurl);
     }
 
