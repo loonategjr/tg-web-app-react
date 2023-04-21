d0db3c65539fd96c2b22c8cbe28eea4256f9d1b1
Styopa Perov <108626279+loonategjr@users.noreply.github.com>
2023-04-21 16:08:19 +0300

    Update ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,48 +2,10 @@ import React, {useState} from 'react';
 import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
 import {useTelegram} from "../../hooks/useTelegram";
-import {useCallback, useEffect} from "react";
 const {products} = useTelegram();
-
-/*
-const getTotalPrice = (items = []) => {
-    return items.reduce((acc, item) => {
-        return acc += item.price
-    }, 0)
-}
-
- */
 const ProductList = () => {
 
-    /*
-    const [addedItems, setAddedItems] = useState([]);
-    const {tg, queryId} = useTelegram();
-
-    const onSendData = useCallback(() => {
-        const data = {
-            products: addedItems,
-            totalPrice: getTotalPrice(addedItems),
-            queryId,
-        }
-        fetch('http://85.119.146.179:8000/web-data', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        })
-    }, [addedItems])
-
-    useEffect(() => {
-        tg.onEvent('mainButtonClicked', onSendData)
-        return () => {
-            tg.offEvent('mainButtonClicked', onSendData)
-        }
-    }, [onSendData])
-
- */
-
-    // тут надо сделать так, чтоб переход был от данного url к нему же + айди продукта
+    tg.MainButton.hide();
 
     const path = window.location.toString();
     const parts = path.split("/");
@@ -52,8 +14,6 @@ const ProductList = () => {
     function redirect(product) {
         window.location.replace(window.location.toString() + "/" + product.id.toString());
     }
-
-    // тут надо сделать product двумерным массивом и поставить индекс id к нему
 
     return (
         <div className={'list'}>
